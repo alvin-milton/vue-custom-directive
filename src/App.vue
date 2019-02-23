90e6da99dc7b72faf98438b8f5653a649f46cbac
Alvin Milton <al@thehandiest.com>
2019-02-23 01:36:12 -0500

    custom directive instead of component just because

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Custom Directives</h1>
+    <p>You can create your own directives if you need to access the DOM for interactions. If you are not doing an interaction, you are likely better off with a component.
+    <br>
+    <br>
+    Here I'm using a custom video directive, which is one way bound to data in the model, and replacing the directive with an element utilizing the data that it is bound to.
+    <br>
+    <br>
+    Probably better off as a component here because a video is not an interaction. Further the component benefits from the its lifecycle, where as I suspect the directive doesn't have the same... 
+    <!-- super late, will get back to this -->
+    </p>
+    <div v-video="video"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      video: {
+        videoID: 'P6g2QbXTpL4',
+        width: 500,
+        height: 315
+      }
+    }
+  },
+  directives: {
+    video: function(el, binding) {
+      el.innerHTML = '<embed width="' + binding.value.width + '" height="' + binding.value.height + '" src="https://www.youtube.com/embed/' + binding.value.videoID + '">';
+    }
   }
 }
 </script>
@@ -21,7 +39,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
